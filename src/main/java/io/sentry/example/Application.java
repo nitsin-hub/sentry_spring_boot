27b9167943799f73009e095c2f6b4366b0ddc9f3
Nitin Singh <nitsin12.uhr@gmail.com>
2020-08-31 12:00:26 +0530

    removed ok from code

--- src/main/java/io/sentry/example/Application.java
+++ src/main/java/io/sentry/example/Application.java
@@ -109,7 +109,7 @@ public class Application {
             // caught exception that will be sent to Sentry
             logger.error("Caught exception!", e);
         }
-        return "Hello Successful OK";
+        return "Hello Successful";
     }
     
     @RequestMapping("/filtered")
